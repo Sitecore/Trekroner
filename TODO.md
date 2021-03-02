ddea634c6fdecf42723699a46851ac4e4a0fd063
Nick Wesselman <nwe@sitecore.net>
2021-03-02 07:53:12 -0500

    initial POC commit

---
+++ TODO.md
@@ -0,0 +1,16 @@
+
+* [ ] Add hosts writer for proxy 
+  * https://github.com/RAhnemann/windows-hosts-writer/blob/master/Program.cs
+* [ ] Codefig for YARP
+* [ ] Custom configuration (for topologies)
+* [ ] Add configuration from environment variable
+* [ ] Error page for failed proxy
+  * [ ] Basic error page for missing container or unable to connect
+  * [ ] Add container logs to error page
+  * [ ] Add container health to error page
+  * [ ] Add dependency health / status to error page
+* [ ] Wait / retry for starting containers
+  * Can this be done in a user-friendly way that would also be compatible with CLI?
+* [ ] Status page
+* [ ] Container actions on status page (e.g. restart)
+* [ ] Notifications?
---
+++ src/Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Sitecore.Trekroner
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddReverseProxy() 
+                .LoadFromConfig(Configuration.GetSection("ReverseProxy"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapReverseProxy(); 
+            });
+        }
+    }
+}
