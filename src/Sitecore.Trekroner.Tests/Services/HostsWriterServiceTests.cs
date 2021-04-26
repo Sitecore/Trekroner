8107c7cd7da8ba163b3a737de2e5e30dbd36a190
Nick Wesselman <nwe@sitecore.net>
2021-04-25 21:52:57 -0400

    fixed tests

--- src/Sitecore.Trekroner.Tests/Services/HostsWriterServiceTests.cs
+++ src/Sitecore.Trekroner.Tests/Services/HostsWriterServiceTests.cs
@@ -11,13 +11,15 @@ using Sitecore.Trekroner.Hosts;
 using Sitecore.Trekroner.Net;
 using Sitecore.Trekroner.HostedServices;
 using Xunit;
+using Microsoft.Extensions.Configuration;
+using Sitecore.Trekroner.Proxy;
 
 namespace Sitecore.Trekroner.Tests.Services
 {
     public class HostsWriterServiceTests
     {
         [Theory, AutoData]
-        public async Task StartAsync_WritesCurrentIp(IPAddress ip, string host)
+        public async Task StartAsync_WritesCurrentIp(IPAddress ip, string host, string defaultDomain)
         {
             var ipResolver = new Mock<IIpAddressResolver>();
             ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
@@ -31,16 +33,17 @@ namespace Sitecore.Trekroner.Tests.Services
                 }
             });
             var hostsWriter = new Mock<IHostsWriter>();
-            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object, hostsWriter.Object, ipResolver.Object);
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
 
             await hostsWriterService.StartAsync(CancellationToken.None);
 
             hostsWriter.Verify(x => x.WriteHosts(
-                It.Is<IEnumerable<HostsEntry>>(y => y.Single().IpAddress == ip.ToString())), Times.Once);
+                It.Is<IEnumerable<HostsEntry>>(y => y.First().IpAddress == ip.ToString())), Times.Once);
         }
 
         [Theory, AutoData]
-        public async Task StartAsync_AddsAllRoutes(IPAddress ip, string host1, string host2)
+        public async Task StartAsync_AddsAllRoutes(IPAddress ip, string host1, string host2, string defaultDomain)
         {
             var ipResolver = new Mock<IIpAddressResolver>();
             ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
@@ -61,7 +64,8 @@ namespace Sitecore.Trekroner.Tests.Services
                 }
             });
             var hostsWriter = new Mock<IHostsWriter>();
-            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object, hostsWriter.Object, ipResolver.Object);
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
 
             await hostsWriterService.StartAsync(CancellationToken.None);
 
@@ -73,7 +77,7 @@ namespace Sitecore.Trekroner.Tests.Services
         }
 
         [Theory, AutoData]
-        public async Task StartAsync_AddsAllHosts(IPAddress ip, string host1, string host2)
+        public async Task StartAsync_AddsAllHosts(IPAddress ip, string host1, string host2, string defaultDomain)
         {
             var ipResolver = new Mock<IIpAddressResolver>();
             ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
@@ -87,19 +91,47 @@ namespace Sitecore.Trekroner.Tests.Services
                 }
             });
             var hostsWriter = new Mock<IHostsWriter>();
-            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object, hostsWriter.Object, ipResolver.Object);
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
 
             await hostsWriterService.StartAsync(CancellationToken.None);
 
             hostsWriter.Verify(x => x.WriteHosts(
                 It.Is<IEnumerable<HostsEntry>>(
-                    y => y.Single().Hosts.Count(z => z == host1) == 1
-                        && y.Single().Hosts.Count(z => z == host2) == 1
+                    y => y.First().Hosts.Count(z => z == host1) == 1
+                        && y.First().Hosts.Count(z => z == host2) == 1
                 )), Times.Once);
         }
 
         [Theory, AutoData]
-        public async Task StartAsync_RemovesBeforeWrites(IPAddress ip, string host)
+        public async Task StartAsync_AddsDefaultDomain(IPAddress ip, string host, string defaultDomain)
+        {
+            var ipResolver = new Mock<IIpAddressResolver>();
+            ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
+            var proxyConfigProvider = GetProxyConfigProvider(new[] {
+                new ProxyRoute
+                {
+                    Match = new ProxyMatch
+                    {
+                        Hosts = new[] { host }
+                    }
+                }
+            });
+            var hostsWriter = new Mock<IHostsWriter>();
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
+
+            await hostsWriterService.StartAsync(CancellationToken.None);
+
+            hostsWriter.Verify(x => x.WriteHosts(
+                It.Is<IEnumerable<HostsEntry>>(
+                    y => y.Count(z => z.Hosts.Single() == host) == 1
+                        && y.Count(z => z.Hosts.Single() == defaultDomain) == 1
+                )), Times.Once);
+        }
+
+        [Theory, AutoData]
+        public async Task StartAsync_RemovesBeforeWrites(IPAddress ip, string host, string defaultDomain)
         {
             var ipResolver = new Mock<IIpAddressResolver>();
             ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
@@ -117,7 +149,8 @@ namespace Sitecore.Trekroner.Tests.Services
             var hostsWriter = new Mock<IHostsWriter>();
             hostsWriter.Setup(x => x.RemoveAll()).Callback(() => removeCalled = true);
             hostsWriter.Setup(x => x.WriteHosts(It.IsAny<IEnumerable<HostsEntry>>())).Callback(() => writeCalledAfter = removeCalled);
-            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object, hostsWriter.Object, ipResolver.Object);
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
 
             await hostsWriterService.StartAsync(CancellationToken.None);
 
@@ -125,7 +158,7 @@ namespace Sitecore.Trekroner.Tests.Services
         }
 
         [Theory, AutoData]
-        public async Task StopAsync_Removes(IPAddress ip, string host)
+        public async Task StopAsync_Removes(IPAddress ip, string host, string defaultDomain)
         {
             var ipResolver = new Mock<IIpAddressResolver>();
             ipResolver.Setup(x => x.GetCurrentIp()).Returns(ip);
@@ -139,7 +172,8 @@ namespace Sitecore.Trekroner.Tests.Services
                 }
             });
             var hostsWriter = new Mock<IHostsWriter>();
-            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object, hostsWriter.Object, ipResolver.Object);
+            var hostsWriterService = new HostsWriterService(proxyConfigProvider.Object,
+                hostsWriter.Object, ipResolver.Object, GetConfiguration(defaultDomain));
 
             await hostsWriterService.StopAsync(CancellationToken.None);
 
@@ -153,6 +187,19 @@ namespace Sitecore.Trekroner.Tests.Services
             var proxyConfigProvider = new Mock<IProxyConfigProvider>();
             proxyConfigProvider.Setup(x => x.GetConfig()).Returns(proxyConfig.Object);
             return proxyConfigProvider;
+        }
+
+        private IConfiguration GetConfiguration(string defaultDomain)
+        {
+            var key = ProxyConfiguration.Key;
+            var inMemorySettings = new Dictionary<string, string>
+            {
+                {$"{key}:DefaultDomain", defaultDomain}
+            };
+
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
         }
     }
 }
